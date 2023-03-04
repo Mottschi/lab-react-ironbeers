b21edf7309b81d60c1d5f94c970e6a9ea69801d0
Sebastian Mottschall <srophkor@gmail.com>
2023-03-04 13:49:09 +0100

    iteration 7, unstyled

--- src/pages/Beers/Beers.jsx
+++ src/pages/Beers/Beers.jsx
@@ -7,18 +7,42 @@ const apiBaseUrl = "https://ih-beers-api2.herokuapp.com/beers";
 
 function Beers() {
   const [beers, setBeers] = useState(null);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    console.log("loading beers");
+    const url = new URL(apiBaseUrl);
+
+    /* if there is no query term, we simply fetch all beers, but if
+       there is one, we change the url to fetch from to the search
+       and add the query parameter
+     */
+    if (query !== "") {
+      url.pathname += "/search";
+      url.searchParams.set("q", query);
+    }
 
     axios
-      .get(apiBaseUrl)
+      .get(url)
       .then((response) => setBeers(response.data))
       .catch((error) => console.log(error));
-  }, []);
-  console.log(beers);
+  }, [query]);
+
+  function handleSearch(event) {
+    setQuery(event.target.value);
+  }
   return (
     <div className="container">
+      <div className="row justify-content-center align-items-center m-3">
+        <div>
+          <label htmlFor="search">Search:</label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            onChange={handleSearch}
+          />
+        </div>
+      </div>
       <div className="row justify-content-center align-items-center">
         {beers ? (
           beers.map((beer) => <BeerCard beer={beer} key={beer._id} />)
